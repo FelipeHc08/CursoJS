5719746e820d2a118bc6f776c3429e809c285700
felipehc.cesar <felipehc.cesar@gmail.com>
2023-08-18 16:53:33 -0300

    Aula 24

---
+++ aula24 (Operador ternário)/index.js
@@ -0,0 +1,13 @@
+// ?:
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+console.log(nivelUsuario);
+
+// if (pontuacaoUsuario >= 1000){
+//     console.log('Usuario VIP');
+// } else{
+//     console.log('Usuario normal');
+// } 
+
